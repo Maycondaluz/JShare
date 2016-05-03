9a8d983548a061e84ea36235384e3badf604ef16
Maycon Vinicius da Luz <maycondaluz2010@hotmail.com>
2016-05-03 01:37:10 -0300

    Criado a tela de interação com o usuário, que inicia o serviço que é o servidor local, e conecta no servidor pai e que possar realizar a pesquisa do arquivo, e realizar a conexão e direto na fonte que possui o download da pesquisa.

---
+++ JShare103982/src/br/maycon/jshare/menu/MenuClienteServ.java
@@ -0,0 +1,222 @@
+package br.maycon.jshare.menu;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
+import java.awt.Color;
+import javax.swing.JTextArea;
+import javax.swing.JTable;
+import java.awt.Font;
+
+public class MenuClienteServ extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txt_user;
+	private JTextField txt_IPServidor;
+	private JTextField txt_portaServidor;
+	private JTextField textField_3;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MenuClienteServ frame = new MenuClienteServ();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MenuClienteServ() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 812, 478);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{102, 138, 0, 13, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 15, 0, 0, 0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JLabel txt_meuIp = new JLabel("New label");
+		GridBagConstraints gbc_txt_meuIp = new GridBagConstraints();
+		gbc_txt_meuIp.gridwidth = 3;
+		gbc_txt_meuIp.insets = new Insets(0, 0, 5, 5);
+		gbc_txt_meuIp.gridx = 0;
+		gbc_txt_meuIp.gridy = 0;
+		contentPane.add(txt_meuIp, gbc_txt_meuIp);
+		
+		JLabel lblDigiteONome = new JLabel("Digite o nome do arquivo:");
+		lblDigiteONome.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		GridBagConstraints gbc_lblDigiteONome = new GridBagConstraints();
+		gbc_lblDigiteONome.anchor = GridBagConstraints.WEST;
+		gbc_lblDigiteONome.insets = new Insets(0, 0, 5, 0);
+		gbc_lblDigiteONome.gridx = 4;
+		gbc_lblDigiteONome.gridy = 0;
+		contentPane.add(lblDigiteONome, gbc_lblDigiteONome);
+		
+		JLabel lblUsurio = new JLabel("Usu\u00E1rio:");
+		GridBagConstraints gbc_lblUsurio = new GridBagConstraints();
+		gbc_lblUsurio.anchor = GridBagConstraints.EAST;
+		gbc_lblUsurio.insets = new Insets(0, 0, 5, 5);
+		gbc_lblUsurio.gridx = 0;
+		gbc_lblUsurio.gridy = 1;
+		contentPane.add(lblUsurio, gbc_lblUsurio);
+		
+		txt_user = new JTextField();
+		GridBagConstraints gbc_txt_user = new GridBagConstraints();
+		gbc_txt_user.insets = new Insets(0, 0, 5, 5);
+		gbc_txt_user.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txt_user.gridx = 1;
+		gbc_txt_user.gridy = 1;
+		contentPane.add(txt_user, gbc_txt_user);
+		txt_user.setColumns(10);
+		
+		textField_3 = new JTextField();
+		GridBagConstraints gbc_textField_3 = new GridBagConstraints();
+		gbc_textField_3.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_3.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_3.gridx = 4;
+		gbc_textField_3.gridy = 1;
+		contentPane.add(textField_3, gbc_textField_3);
+		textField_3.setColumns(10);
+		
+		JLabel lblIpDoServidor = new JLabel("IP do servidor:");
+		GridBagConstraints gbc_lblIpDoServidor = new GridBagConstraints();
+		gbc_lblIpDoServidor.anchor = GridBagConstraints.EAST;
+		gbc_lblIpDoServidor.insets = new Insets(0, 0, 5, 5);
+		gbc_lblIpDoServidor.gridx = 0;
+		gbc_lblIpDoServidor.gridy = 2;
+		contentPane.add(lblIpDoServidor, gbc_lblIpDoServidor);
+		
+		txt_IPServidor = new JTextField();
+		GridBagConstraints gbc_txt_IPServidor = new GridBagConstraints();
+		gbc_txt_IPServidor.insets = new Insets(0, 0, 5, 5);
+		gbc_txt_IPServidor.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txt_IPServidor.gridx = 1;
+		gbc_txt_IPServidor.gridy = 2;
+		contentPane.add(txt_IPServidor, gbc_txt_IPServidor);
+		txt_IPServidor.setColumns(10);
+		
+		JButton bt_pesquisar = new JButton("Pesquisar arquivo");
+		GridBagConstraints gbc_bt_pesquisar = new GridBagConstraints();
+		gbc_bt_pesquisar.fill = GridBagConstraints.HORIZONTAL;
+		gbc_bt_pesquisar.insets = new Insets(0, 0, 5, 0);
+		gbc_bt_pesquisar.gridx = 4;
+		gbc_bt_pesquisar.gridy = 2;
+		contentPane.add(bt_pesquisar, gbc_bt_pesquisar);
+		
+		JLabel lblPortaDoServidor = new JLabel("Porta do servidor:");
+		GridBagConstraints gbc_lblPortaDoServidor = new GridBagConstraints();
+		gbc_lblPortaDoServidor.anchor = GridBagConstraints.EAST;
+		gbc_lblPortaDoServidor.insets = new Insets(0, 0, 5, 5);
+		gbc_lblPortaDoServidor.gridx = 0;
+		gbc_lblPortaDoServidor.gridy = 3;
+		contentPane.add(lblPortaDoServidor, gbc_lblPortaDoServidor);
+		
+		txt_portaServidor = new JTextField();
+		GridBagConstraints gbc_txt_portaServidor = new GridBagConstraints();
+		gbc_txt_portaServidor.insets = new Insets(0, 0, 5, 5);
+		gbc_txt_portaServidor.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txt_portaServidor.gridx = 1;
+		gbc_txt_portaServidor.gridy = 3;
+		contentPane.add(txt_portaServidor, gbc_txt_portaServidor);
+		txt_portaServidor.setColumns(10);
+		
+		JButton bt_conectar = new JButton("Conectar no servidor");
+		GridBagConstraints gbc_bt_conectar = new GridBagConstraints();
+		gbc_bt_conectar.insets = new Insets(0, 0, 5, 5);
+		gbc_bt_conectar.gridx = 2;
+		gbc_bt_conectar.gridy = 3;
+		contentPane.add(bt_conectar, gbc_bt_conectar);
+		
+		JButton bt_baixar = new JButton("baixar arquivo");
+		bt_baixar.setToolTipText("selecione um linha da pesquisa pra download");
+		GridBagConstraints gbc_bt_baixar = new GridBagConstraints();
+		gbc_bt_baixar.fill = GridBagConstraints.HORIZONTAL;
+		gbc_bt_baixar.insets = new Insets(0, 0, 5, 0);
+		gbc_bt_baixar.gridx = 4;
+		gbc_bt_baixar.gridy = 3;
+		contentPane.add(bt_baixar, gbc_bt_baixar);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.insets = new Insets(0, 0, 5, 5);
+		gbc_scrollPane.gridwidth = 3;
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.gridx = 0;
+		gbc_scrollPane.gridy = 4;
+		contentPane.add(scrollPane, gbc_scrollPane);
+		
+		JTextArea txtA_servicoLocal = new JTextArea();
+		scrollPane.setViewportView(txtA_servicoLocal);
+		
+		JSeparator separator = new JSeparator();
+		separator.setRequestFocusEnabled(false);
+		separator.setBackground(Color.BLACK);
+		separator.setForeground(Color.BLACK);
+		GridBagConstraints gbc_separator = new GridBagConstraints();
+		gbc_separator.gridheight = 5;
+		gbc_separator.insets = new Insets(0, 0, 0, 5);
+		gbc_separator.gridx = 3;
+		gbc_separator.gridy = 1;
+		contentPane.add(separator, gbc_separator);
+		
+		JScrollPane table_pesquisa = new JScrollPane();
+		GridBagConstraints gbc_table_pesquisa = new GridBagConstraints();
+		gbc_table_pesquisa.insets = new Insets(0, 0, 5, 0);
+		gbc_table_pesquisa.fill = GridBagConstraints.BOTH;
+		gbc_table_pesquisa.gridx = 4;
+		gbc_table_pesquisa.gridy = 4;
+		contentPane.add(table_pesquisa, gbc_table_pesquisa);
+		
+		table = new JTable();
+		table_pesquisa.setViewportView(table);
+		
+		JButton bt_encerraConComServidor = new JButton("Desconectar do servidor");
+		GridBagConstraints gbc_bt_encerraConComServidor = new GridBagConstraints();
+		gbc_bt_encerraConComServidor.anchor = GridBagConstraints.EAST;
+		gbc_bt_encerraConComServidor.insets = new Insets(0, 0, 0, 5);
+		gbc_bt_encerraConComServidor.gridx = 1;
+		gbc_bt_encerraConComServidor.gridy = 5;
+		contentPane.add(bt_encerraConComServidor, gbc_bt_encerraConComServidor);
+		
+		JButton bt_encerraServico = new JButton("Encerrar servi\u00E7o");
+		GridBagConstraints gbc_bt_encerraServico = new GridBagConstraints();
+		gbc_bt_encerraServico.anchor = GridBagConstraints.EAST;
+		gbc_bt_encerraServico.insets = new Insets(0, 0, 0, 5);
+		gbc_bt_encerraServico.gridx = 2;
+		gbc_bt_encerraServico.gridy = 5;
+		contentPane.add(bt_encerraServico, gbc_bt_encerraServico);
+		
+		JButton bt_sair = new JButton("Sair");
+		GridBagConstraints gbc_bt_sair = new GridBagConstraints();
+		gbc_bt_sair.anchor = GridBagConstraints.EAST;
+		gbc_bt_sair.gridx = 4;
+		gbc_bt_sair.gridy = 5;
+		contentPane.add(bt_sair, gbc_bt_sair);
+	}
+
+}
--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 + Criado o método para retorn IP da máquina
 + Na classe ListarDiretoriosArquivos foi criado o método listarArquivos
 + na mesma foi criada o método listar diretórios
+
++ Criado uma JFrame -> MenuClienteServ para cliente servidor
++ Criado Menu do usuário, a interface grafica para usuário poder se conectar na máquina de outra pessoa, para que possa realiazar download
